f5e13078e4fedd02bf0bb3d9e48555378e4b400e
Ben Diegel <b.diegelg@gmail.com>
2017-07-30 12:15:15 -0400

    Finalized class choosing

--- AdventureMain.py
+++ AdventureMain.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import random
 
 class NPCNames:
@@ -6,9 +8,12 @@ class NPCNames:
         self.king = "Wilhye"
 NPC = NPCNames()
 
+player = [""]
+
 class Warrior:
     def __init__(self, name):
         self.name = name
+        self.cName = "warrior"
         self.hp = 150
         self.ar = 4
         self.str = 3
@@ -17,6 +22,7 @@ class Warrior:
 class Wizard:
     def __init__(self, name):
         self.name = name
+        self.cName = "wizard"
         self.hp = 75
         self.ar = 2
         self.str = 1
@@ -25,6 +31,7 @@ class Wizard:
 class Rouge:
     def __init__(self, name):
         self.name = name
+        self.cName = "rouge"
         self.hp = 100
         self.ar = 2
         self.str = 4
@@ -32,6 +39,7 @@ class Rouge:
         self.int = 2
 
 def startGame(NPC):
+    print(NPC.narrator + ":")
     print("   Hello weary traveler, I hope your adventure has served you well.\n"
           "   My name is %s I am Tavilon's travel guide and will be helping\n"
           "   you prepare for vast adventures that await. King %s needs brave\n"
@@ -42,21 +50,73 @@ def startGame(NPC):
           "\n"
           "   So what will it be hero? 'warrior', 'wizard', or ' rouge'." % (NPC.narrator, NPC.king))
 
-
-def heroClass():
+def heroClass(player):
     userClass = input(">> ")
     if userClass == 'warrior':
         userInput = input("   So, you want to be a warrior? (y/n)\n>>")
         if userInput == 'y' or userInput == 'Y':
             userName = input("   What is your name hero?\n>>")
-
+            player[0] = Warrior(userName)
+            firstAct(NPC, player)
         elif userInput =='n' or userInput == 'N':
             print("   Alright, 'warrior', 'wizard', or ' rouge'.")
-            heroClass()
-
-def classFunction(userName):
-    player = Warrior(userName)
-    return player
-
+            heroClass(player)
+        else:
+            print("   I do not understand you.")
+            print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+            heroClass(player)
+    elif userClass == 'wizard':
+        userInput = input("   So, you want to be a wizard? (y/n)\n>>")
+        if userInput == 'y' or userInput == 'Y':
+            userName = input("   What is your name hero?\n>>")
+            player[0] = Wizard(userName)
+            firstAct(NPC, player)
+        elif userInput =='n' or userInput == 'N':
+            print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+            heroClass(player)
+        else:
+            print("   I do not understand you.")
+            print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+            heroClass(player)
+    elif userClass == 'rouge':
+        userInput = input("   So, you want to be a rouge? (y/n)\n>>")
+        if userInput == 'y' or userInput == 'Y':
+            userName = input("   What is your name hero?\n>>")
+            player[0] = Rouge(userName)
+            firstAct(NPC, player)
+        elif userInput =='n' or userInput == 'N':
+            print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+            heroClass(player)
+        else:
+            print("   I do not understand you.")
+            print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+            heroClass(player)
+    else:
+        print("   I do not understand you.")
+        print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+        heroClass(player)
+        
+def firstAct(NPC, p):
+    print(NPC.narrator + ":")
+    print("   Hello %s, you chose to be a %s. That is a mighty class with\n"
+          "   %s health\n"
+          "   %s armor\n"
+          "   %s strenth\n"
+          "   %s speed\n"
+          "   %s intelligence\n\n"
+          "   These values will change over the course of your travels, so prepare\n"
+          "   for growth!"
+          % (p[0].name, p[0].cName, p[0].hp, p[0].ar, p[0].str, p[0].spd, p[0].int))
+    userInput = input("   Do you like these stats? (y/n)\n>>")
+    if userInput == 'n' or userInput == 'N':
+        print(NPC.narrator + ":")
+        print("   What would you like to change your class too?")
+        print("   Alright, 'warrior', 'wizard', or ' rouge'.")
+        heroClass(player)
+    elif userInput == 'y' or userInput == 'Y':
+        print("   Welcome courageous %s" % (p[0].cName))
+    
 startGame(NPC)
-heroClass()
+heroClass(player)
+
+
